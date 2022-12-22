21a7863d0c42a80939ae787c522e0d85f3704b71
hotsix <codinggo@naver.com>
2022-12-23 01:56:30 +0900

    (1) 기본 틀

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>헤더입니다</header>
+      <main>
+        {/* input 관련 */}
+        <input />
+        <input />
+        <button>추가</button>
+        {/* TodoList 관련 */}
+      </main>
+      <footer>푸터입니다</footer>
+    </div>
+  );
+}
+
+export default App;
